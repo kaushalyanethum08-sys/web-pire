8dd800c55750f03e63adbe88bea0db3cdfd9da1e
kaushalyanethum08-sys <kaushalyanethum08@gmail.com>
2026-02-08 16:18:10 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'kaushalyanethum08@gmail.com',
+    password: 'kaushalyanethum08@gmail.com',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
